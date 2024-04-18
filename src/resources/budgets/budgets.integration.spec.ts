c61d50d5d5a3d49807db69225e35de7f529f594c
Thiago Elias <thiagoelias99@gmail.com>
2024-04-18 09:47:43 -0300

    feat: :test_tube: Add Budget Post Test

---
+++ src/resources/budgets/budgets.integration.spec.ts
@@ -0,0 +1,117 @@
+import { INestApplication, ValidationPipe } from '@nestjs/common'
+import { Test, TestingModule } from '@nestjs/testing'
+import * as request from 'supertest'
+import { useContainer } from 'class-validator'
+
+import { BudgetsController } from './budgets.controller'
+import { BudgetsService } from './budgets.service'
+import { AppModule } from '../../app.module'
+import { CreateBudgetDto } from './dto/create-budget.dto'
+import { Budget, BudgetPaymentMethodEnum } from './budgets.entity'
+import { CreateUserDto } from '../users/dto/create-user.dto'
+
+describe('BudgetsIntegration', () => {
+  let app: INestApplication
+  let accessToken: string
+
+  beforeAll(async () => {
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      imports: [AppModule]
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+    app.useGlobalPipes(
+      new ValidationPipe({
+        transform: true,
+        whitelist: true,
+        forbidNonWhitelisted: true,
+      }),
+    )
+    useContainer(app.select(AppModule), { fallbackOnErrors: true })
+
+    await app.init()
+
+    //Register Test User
+    const userLoginData = CreateUserDto.mock()
+    await request(app.getHttpServer())
+      .post('/signup')
+      .send(userLoginData)
+
+    //Login & Get JWT
+    const response = await request(app.getHttpServer())
+      .post('/login')
+      .send({ password: userLoginData.password, email: userLoginData.email })
+    accessToken = response.body.accessToken
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  describe('POST /budgets', () => {
+    it('should create a budget with only value', async () => {
+      const createData: CreateBudgetDto = {
+        value: 1000.99,
+      }
+
+      const response = await request(app.getHttpServer())
+        .post('/budgets')
+        .set('Authorization', `Bearer ${accessToken}`)
+        .send(createData)
+        .expect(201)
+
+      expect(response.body).toHaveProperty('id')
+      expect(response.body).toHaveProperty('currentValue')
+      expect(response.body.currentValue).toBe(createData.value)
+      expect(response.body.isRecurrent).toBeFalsy()
+      expect(response.body).toHaveProperty('recurrenceHistory')
+      expect(response.body.recurrenceHistory).toHaveProperty('registers')
+      expect(response.body.recurrenceHistory.registers).toBeInstanceOf(Array)
+      expect(response.body.recurrenceHistory.registers).toHaveLength(1)
+      expect(response.body.recurrenceHistory.registers[0]).toHaveProperty('value')
+      expect(response.body.recurrenceHistory.registers[0].value).toBe(createData.value)
+      expect(response.body.description).toBe('Extra')
+    })
+
+    it('should create a budget with all fields', async () => {
+      const createData: CreateBudgetDto = {
+        value: 1000.99,
+        description: 'Test',
+        consolidated: false,
+        startDate: new Date('2021-01-01'),
+        endDate: new Date(),
+        paymentMethod: BudgetPaymentMethodEnum.PIX,
+        isIncome: false,
+      }
+
+      const response = await request(app.getHttpServer())
+        .post('/budgets')
+        .set('Authorization', `Bearer ${accessToken}`)
+        .send(createData)
+        .expect(201)
+
+      expect(response.body).toHaveProperty('id')
+      expect(response.body.currentValue).toBe(createData.value)
+      expect(response.body.description).toBe(createData.description)
+      expect(response.body.isRecurrent).toBeTruthy()
+
+      expect(response.body).toHaveProperty('recurrenceHistory')
+      expect(response.body.recurrenceHistory).toHaveProperty('registers')
+      expect(response.body.recurrenceHistory.registers).toBeInstanceOf(Array)
+      expect(response.body.recurrenceHistory.registers).toHaveLength(1)
+      expect(response.body.recurrenceHistory.registers[0]).toHaveProperty('value')
+      expect(response.body.recurrenceHistory.registers[0].value).toBe(createData.value)
+
+      expect(response.body.recurrenceHistory).toHaveProperty('activePeriods')
+      expect(response.body.recurrenceHistory.activePeriods).toBeInstanceOf(Array)
+      expect(response.body.recurrenceHistory.activePeriods).toHaveLength(1)
+      expect(response.body.recurrenceHistory.activePeriods[0]).toHaveProperty('startDate')
+      expect(response.body.recurrenceHistory.activePeriods[0].startDate).toBe(createData.startDate.toISOString())
+      expect(response.body.recurrenceHistory.activePeriods[0]).toHaveProperty('endDate')
+      expect(response.body.recurrenceHistory.activePeriods[0].endDate).toBe(createData.endDate.toISOString())
+
+
+
+    })
+  })
+})
--- src/resources/budgets/budgets.service.ts
+++ src/resources/budgets/budgets.service.ts
@@ -13,6 +13,8 @@ export class BudgetsService {
   async create(userId: string, data: CreateBudgetDto) {
     let isIncome: boolean = true
     let consolidated: boolean = true
+    let startDate: Date | undefined
+    let expectedDay: number
 
     // Normalize isIncome
     if (data.isIncome === undefined) {
@@ -28,12 +30,28 @@ export class BudgetsService {
       consolidated = data.consolidated
     }
 
+    // Normalize startDate
+    if (data.startDate) {
+      startDate = new Date(data.startDate)
+    }
+
+    // Normalize expectedDay
+    if (data.expectedDay) {
+      expectedDay = data.expectedDay
+    } else if (startDate) {
+      expectedDay = startDate.getDate()
+    } else {
+      expectedDay = new Date().getDate()
+    }
+
+    console.log('Creating budget with data:', data.startDate)
+
     const createdBudget = await this.budgetsRepository.create({
       id: randomUUID(),
       userId,
       isRecurrent: !!data.startDate,
       description: data.description || 'Extra',
-      expectedDay: data.expectedDay || data.startDate?.getDate() || new Date().getDate(),
+      expectedDay: expectedDay,
       isIncome,
       paymentMethod: data.paymentMethod || BudgetPaymentMethodEnum.DEBIT,
       value: data.value,
@@ -41,7 +59,7 @@ export class BudgetsService {
       recurrenceHistory: {
         activePeriods: [
           {
-            startDate: data.startDate,
+            startDate: startDate,
             endDate: data.endDate
           }
         ],
